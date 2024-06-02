41a59a9021f765a06177390cc3270368b178b690
LLLLLyf <lyf1714791684@163.com>
2024-06-02 21:55:32 +0800

    rebase
    'app'

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const fs = require('fs')
+
 const Path  = require('path')
-const {PORT} =  require('./config/config_env')
+const {PORT,JWT_SECRET} =  require('./config/config_env')
 
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+//jwt
 const jwt = require('@fastify/jwt')
-
+fastify.register(jwt,{secret:JWT_SECRET})
 
 //数据库实例
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 
-fastify.get('/', function handler (request, reply) {
-    reply.send({ hello: 'world' })
+fastify.post('/v1/market/login', (request, reply) =>{
+    const {name} = request.body 
+    const token  = fastify.jwt.sign({name})
+    reply.send({code:0,token,username:name,msg:'登陆成功' })
 })
 
 fastify.post('/v1/market/register',async(request,reply)=>{
